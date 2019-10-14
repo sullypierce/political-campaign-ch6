57f08d2d977f547cbb4db4e9e03a9608d1bf0ab6
Sullivan Pierce <sullypierce@gmail.com>
2019-10-14 11:01:10 -0500

    wrote functions to modify campaign object

--- main.js
+++ main.js
@@ -28,3 +28,15 @@ const elizabethsCampaign = {
     missionStatement: "Don't ever, for any reason, do anything to anyone for any reason ever, no matter what, no matter where, or who, or who you are with, or where you are going, or where you've been... ever, for any reason whatsoever...",
     registerURL: "getyoselftothepolls.gov"
 }
+
+addToImageGallery = function(imageName, newImage) {
+    elizabethsCampaign.images[imageName] = newImage;
+};
+
+changeBiography = function(newBiography) {
+    elizabethsCampaign.bio = newBiography;
+};
+
+changePlatform = (topic, newPlatformStatement) => {
+    elizabethsCampaign.platform[topic] = newPlatformStatement;
+};
